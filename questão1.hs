6d8d6475072ac627d24b1095173a844dbe7668c3
Felipe Macedo <147464434+Lefpe@users.noreply.github.com>
2024-04-02 00:52:53 -0300

    Update questão1.hs

--- questão1.hs
+++ questão1.hs
@@ -1,1 +1,13 @@
+potencia :: Int -> Int -> Int
+potencia _ 0 = 1
+potencia base expoente = base * potencia base (expoente - 1)
 
+main :: IO ()
+main = do
+  putStrLn "Digite a base:"
+  baseStr <- getLine
+  putStrLn "Digite o expoente:"
+  expoenteStr <- getLine
+  let base = read baseStr :: Int
+      expoente = read expoenteStr :: Int
+  putStrLn $ "O resultado da potência é: " ++ show (potencia base expoente)
