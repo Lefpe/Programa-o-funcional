5efeef1e6f7ee155963a85082a879612d82eb424
Felipe Macedo <147464434+Lefpe@users.noreply.github.com>
2024-04-09 10:11:01 -0300

    Update atvq1(n2).hs

--- atvq1(n2).hs
+++ atvq1(n2).hs
@@ -1,1 +1,19 @@
+module Main where
+--definir potencia 
+potencia :: Int -> Int -> Int
+potencia_0 = 1
+potencia base expoente = base * potencia base (expoente - 1)
+--estrutura principal 
+main :: IO ()
+main = do
+--erro aqui
+putStrLn "Digite a base:"
+baseStr <- getLine 
+putStrLn "Digite o expoente:"
+expoente <- getLine
+let resultado = potencia base expoente
+if expoente < 0 
+then putStrLn "O expoente deve ser não negativo."
+else do
+putStrLn ("O resultado de " ++ show base ++ " elevado a " ++ show resultado 
 
